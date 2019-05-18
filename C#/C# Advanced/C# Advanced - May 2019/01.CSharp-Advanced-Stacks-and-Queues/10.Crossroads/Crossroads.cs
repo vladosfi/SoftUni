352e09fd84de5bb9e7befdedd27be0100f03624c
vladosfi <sfi@abv.bg>
2019-05-18 21:46:26 +0300

    Upload C# Advanced-Jan-2019- List and Queue - Task 10

--- C#/C# Advanced/C# Advanced - May 2019/01.CSharp-Advanced-Stacks-and-Queues/10.Crossroads/Crossroads.cs
+++ C#/C# Advanced/C# Advanced - May 2019/01.CSharp-Advanced-Stacks-and-Queues/10.Crossroads/Crossroads.cs
@@ -15,39 +15,39 @@ namespace _10.Crossroads
 
             while (input.ToLower() != "end")
             {
-                if (input.ToLower() == "green")
-                {
-                    int curTime = greenLight;
-                    string curCar;
-                    string carInCrossroad = string.Empty;
-
-                    while (curTime > 0 && cars.Count > 0)
-                    {
-                        curCar = cars.Dequeue();
-                        totalCarsPassed++;
-
-                        if (curCar.Length <= curTime)
-                        {
-                            curTime -= curCar.Length;
-                        }
-                        else
-                        {
-                            carInCrossroad = curCar.Substring(curTime, curCar.Length - curTime);
-                            curTime = 0;
-
-                            if (freeWindow < carInCrossroad.Length)
-                            {
-                                char hitPlace = carInCrossroad[freeWindow];
-                                Console.WriteLine("A crash happened!");
-                                Console.WriteLine($"{curCar} was hit at {hitPlace}.");
-                                return;
-                            }
-                        }
-                    }
-                }
-                else
+                if (input.ToLower() != "green")
                 {
                     cars.Enqueue(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                int curTime = greenLight;
+                string curCar;
+                string carInCrossroad = string.Empty;
+
+                while (curTime > 0 && cars.Count > 0)
+                {
+                    curCar = cars.Dequeue();
+                    totalCarsPassed++;
+
+                    if (curCar.Length <= curTime)
+                    {
+                        curTime -= curCar.Length;
+                    }
+                    else
+                    {
+                        carInCrossroad = curCar.Substring(curTime, curCar.Length - curTime);
+                        curTime = 0;
+
+                        if (freeWindow < carInCrossroad.Length)
+                        {
+                            char hitPlace = carInCrossroad[freeWindow];
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{curCar} was hit at {hitPlace}.");
+                            return;
+                        }
+                    }
                 }
 
                 input = Console.ReadLine();

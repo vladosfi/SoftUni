8f3f1e05cc717d4eb1e5f7a295e75a816a9d7c76
vladosfi <sfi@abv.bg>
2019-05-18 13:32:48 +0300

    Upload C# Advanced-Jan-2019- List and Queue - Task 7

--- C#/C# Advanced/C# Advanced - May 2019/01.CSharp-Advanced-Stacks-and-Queues/07.TruckTour/TruckTour.cs
+++ C#/C# Advanced/C# Advanced - May 2019/01.CSharp-Advanced-Stacks-and-Queues/07.TruckTour/TruckTour.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.TruckTour
 {
-    class Program
+    class TruckTour
     {
         static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int[]> pumps = new Queue<int[]>();
+            int indexOfPomp = 0;
 
+            for (int i = 0; i < n; i++)
+            {
+                int[] petrolPump = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                pumps.Enqueue(petrolPump);
+            }
+
+            while (true)
+            {
+                int tankAmount = 0;
+
+                foreach (var pump in pumps)
+                {
+                    int[] tokens = pump;
+                    int fuelAmount = pump[0];
+                    int distance = pump[1];
+                    tankAmount += fuelAmount - distance;
+
+                    if (tankAmount < 0)
+                    {
+                        pumps.Enqueue(pumps.Dequeue());
+                        indexOfPomp++;
+                        break;
+                    }
+                }
+
+                if (tankAmount >= 0)
+                {
+                    break;
+                }
+            }
+
+
+
+            Console.WriteLine(indexOfPomp);
         }
     }
 }

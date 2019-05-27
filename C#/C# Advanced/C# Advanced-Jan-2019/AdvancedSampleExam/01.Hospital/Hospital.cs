55ec526552a6549b770ed3352694574a313664a0
vladosfi <vladosfi@gmail.com>
2019-05-27 17:21:53 +0300

    Exam Test1

---
+++ C#/C# Advanced/C# Advanced-Jan-2019/AdvancedSampleExam/01.Hospital/Hospital.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Hospital
+{
+    public class Hospital
+    {
+        List<Departmet> departments;
+
+        public Hospital(string departmentName, string doctorName, string patientName)
+        {
+            if (!departments.name)
+            {
+
+            }
+        }
+
+        private class Doctor
+        {
+            public Doctor(string name)
+            {
+                this.Name = name;
+            }
+
+            public string Name { get; }
+        }
+    }
+
+    public class Departmet
+    {
+        private string name;
+        private Room[] rooms;
+        
+        private class Room
+        {
+            private int roomsCount;
+
+            private class patients
+            {
+                private int count;
+                private string name;
+
+
+            }
+        }
+
+    }
+
+    class HospitalTask
+    {
+        static void Main()
+        {
+
+        }
+    }
+}

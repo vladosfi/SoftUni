8dda34ab40062c993aecb27dd02a1c9ef2e84fc2
vladosfi <vladosfi@gmail.com>
2019-05-29 17:35:33 +0300

    Exam Test1 task 13

---
+++ C#/C# Advanced/C# Advanced-Jan-2019/SampleExam/SoftUniParking/Car.cs
@@ -0,0 +1,25 @@
+﻿namespace SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.make = make;
+            this.model = model;
+            this.horsePower = horsePower;
+            this.registrationNumber = registrationNumber;
+        }
+
+        public string Make => this.make;
+        public string Model => this.model;
+        public int HorsePower => this.horsePower;
+        public string RegistrationNumber => this.registrationNumber;
+
+
+    }
+}
---
+++ C#/C# Advanced/C# Advanced-Jan-2019/SampleExam/SoftUniParking/Parking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    class Parking
+    {
+        private Car[] cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.cars = new Car[this.capacity];
+        }
+
+
+    }
+}

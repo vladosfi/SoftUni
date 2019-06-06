cce24fe6b77fb0f6b29299ba57938a8a425d228e
vladosfi <vladosfi@gmail.com>
2019-06-06 08:35:54 +0300

    Func Pprogramming exercise complete

--- C#/C# Advanced/C# Advanced - May 2019/05.Functional-Programming/11.PartyReservationFilterModule/PartyReservationFilterModule.cs
+++ C#/C# Advanced/C# Advanced - May 2019/05.Functional-Programming/11.PartyReservationFilterModule/PartyReservationFilterModule.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11.PartyReservationFilterModule
 {
@@ -6,7 +8,61 @@ namespace _11.PartyReservationFilterModule
     {
         static void Main()
         {
+            Func<string, string, bool> startsWith = (name, startText) => !name.StartsWith(startText);
+            Func<string, string, bool> endWith = (name, startText) => !name.EndsWith(startText);
+            Func<string, int, bool> lengthWith = (name, len) => name.Length != len;
+            Func<string, string, bool> contains = (name, text) => !name.Contains(text);
+            Action<string[]> print = guestsComming => Console.WriteLine(string.Join(" ", guestsComming));
 
+            string[] names = Console.ReadLine()
+                .Split();
+
+            string filter = Console.ReadLine();
+            List<string> filters = new List<string>();
+
+            while (filter!= "Print")
+            {
+                string[] filterInfo = filter.Split(";");
+                string action = filterInfo[0];
+
+                if (action == "Add filter")
+                {
+                    filters.Add($"{filterInfo[1]};{filterInfo[2]}");
+                }
+                else
+                {
+                    filters.Remove($"{filterInfo[1]};{filterInfo[2]}");
+                }
+
+                filter = Console.ReadLine();
+            }
+
+            foreach (var currentFilter in filters)
+            {
+                string[] currentFilterInfo = currentFilter.Split(";");
+                string action = currentFilterInfo[0];
+                string actionParam = currentFilterInfo[1];
+
+
+                if (action == "Starts with")
+                {
+                    names = names.Where(n => startsWith(n, actionParam)).ToArray();
+                }
+                else if (action == "Ends with")
+                {
+                    names = names.Where(n => endWith(n, actionParam)).ToArray();
+                }
+                else if (action == "Length")
+                {
+                    names = names.Where(n => lengthWith(n, int.Parse(actionParam))).ToArray();
+                }
+                else if (action == "Contains")
+                {
+                    names = names.Where(n => contains(n, actionParam)).ToArray();
+                }
+            }
+
+            print(names);
         }
     }
 }
---
+++ C#/C# Advanced/C# Advanced - May 2019/05.Functional-Programming/13.TriFunction/TriFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _13.TriFunction
+{
+    class TriFunction
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isLarger = (string name, int length)
+                => name.Sum(c => c) >= length;
+
+            Func<string[], Func<string, int, bool>, string> nameFilter =
+                (inputNames, isLargerFilter) 
+                => inputNames.FirstOrDefault(x => isLargerFilter(x,n));
+
+            string resultName = nameFilter(names, isLarger);
+
+            Console.WriteLine(resultName);
+        }
+    }
+}

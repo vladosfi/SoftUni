117bd4fbbb5608ad01e5acb234ef581528e86cd9
vladosfi <vladosfi@gmail.com>
2019-06-14 16:01:38 +0300

    Defining classes

---
+++ C#/C# Advanced/C# Advanced - May 2019/08.Generics-Lab/01.BoxofT/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> boxes;
+
+        public int Count { get => boxes.Count;}
+
+        public Box()
+        {
+            boxes = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            boxes.Add(element);
+        }
+
+        public T Remove()
+        {
+            if (boxes.Count > 0)
+            {
+                T elementToRemove = boxes[boxes.Count - 1];
+                boxes.RemoveAt(boxes.Count - 1);
+                return elementToRemove;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+        }
+    }
+}
---
+++ C#/C# Advanced/C# Advanced - May 2019/08.Generics-Lab/03.GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equality = new EqualityScale<int>(5, 5);
+
+            Console.WriteLine(equality.AreEqual());
+        }
+    }
+}

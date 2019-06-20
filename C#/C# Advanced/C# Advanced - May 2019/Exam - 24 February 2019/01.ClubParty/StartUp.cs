ea918adf8c5789bbbf3fb02879a2911ee16f5657
vladosfi <vladosfi@gmail.com>
2019-06-20 17:46:45 +0300

    Exam preparations

---
+++ C#/C# Advanced/C# Advanced - May 2019/Exam - 24 February 2019/01.ClubParty/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ClubParty
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int hallsCapacity = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Stack<string> elements = new Stack<string>(input);
+            List<int> allGroup = new List<int>();
+            Queue<string> halls = new Queue<string>();
+
+            while (elements.Count > 0)
+            {
+                string element = elements.Pop();
+
+                if (int.TryParse(element,out int company))
+                {
+                    if (halls.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    if (allGroup.Sum() + company > hallsCapacity)
+                    {
+                        Console.WriteLine($"{halls.Dequeue()} -> {string.Join(", ", allGroup)}");
+                        allGroup.Clear();
+                    }
+
+                    if (halls.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    allGroup.Add(company);
+                }
+                else
+                {
+                    halls.Enqueue(element);
+                }                
+            }
+
+
+        }
+    }
+}
---
+++ C#/C# Advanced/C# Advanced - May 2019/Exam - 24 February 2019/02.TronRacers/StartUp.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace _02.TronRacers
+{
+    class StartUp
+    {
+        static char[][] matrix;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());            
+            int[] firstPalyerPosition = new int[2];
+            int[] secondPalyerPosition = new int[2];
+            matrix = new char[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+                matrix[row] = new char[n];
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row][col] = input[col];
+
+                    if (input[col] == 'f')
+                    {
+                        firstPalyerPosition[0] = row;
+                        firstPalyerPosition[1] = col;
+                    }
+                    else if (input[col] == 's')
+                    {
+                        secondPalyerPosition[0] = row;
+                        secondPalyerPosition[1] = col;
+                    }
+                }
+            }
+
+
+            while (true)
+            {
+
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (MovePlayer(command, 0, firstPalyerPosition) ||
+                    MovePlayer(command, 1, secondPalyerPosition))
+                {
+                    break;
+                }
+            }
+
+            
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+
+        }
+
+        private static bool MovePlayer(string[] command, int player, int[] playerPosition)
+        {
+            bool isPlayerDead = false;
+            int row = playerPosition[0];
+            int col = playerPosition[1];
+            char playerSymbol = matrix[row][col];
+
+            switch (command[player])
+            {
+                case "up":
+
+                    playerPosition[0]--;
+
+                    if (GoesOut(playerPosition))
+                    {
+                        playerPosition[0] = matrix.Length - 1;
+                    }
+
+                    if (IsDead(playerPosition))
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = 'x';
+                        isPlayerDead = true;
+                    }
+                    else
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = playerSymbol;
+                    }                    
+                    break;
+                case "down":
+
+                    playerPosition[0]++;
+
+                    if (GoesOut(playerPosition))
+                    {
+                        playerPosition[0] = 0;
+                    }
+
+                    if (IsDead(playerPosition))
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = 'x';
+                        isPlayerDead = true;
+                    }
+                    else
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = playerSymbol;
+                    }
+                    break;
+                case "left":
+                    playerPosition[1]--;
+
+                    if (GoesOut(playerPosition))
+                    {
+                        playerPosition[1] = matrix[playerPosition[0]].Length - 1;
+                    }
+
+                    if (IsDead(playerPosition))
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = 'x';
+                        isPlayerDead = true;
+                    }
+                    else
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = playerSymbol;
+                    }
+                    break;
+                case "right":
+                    playerPosition[1]++;
+
+                    if (GoesOut(playerPosition))
+                    {
+                        playerPosition[1] = 0;
+                    }
+
+                    if (IsDead(playerPosition))
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = 'x';
+                        isPlayerDead = true;
+                    }
+                    else
+                    {
+                        matrix[playerPosition[0]][playerPosition[1]] = playerSymbol;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return isPlayerDead;
+        }
+
+        private static bool IsDead(int[] playerPosition)
+        {
+            bool result = false;
+
+            if (matrix[playerPosition[0]][playerPosition[1]] != '*')
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        private static bool GoesOut(int[] palyerPosition)
+        {
+            bool result = true;
+
+            if (palyerPosition[0] >= 0 && 
+                palyerPosition[1] >= 0 &&
+                palyerPosition[0] < matrix.Length && 
+                palyerPosition[1] < matrix[palyerPosition[0]].Length)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}

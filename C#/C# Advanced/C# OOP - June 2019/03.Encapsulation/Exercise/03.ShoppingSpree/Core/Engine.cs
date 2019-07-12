d2ce84f0557a8f5c6128b11d500ca5878416cebc
vladosfi <vladosfi@gmail.com>
2019-07-12 09:20:07 +0300

    Encapsulation - Exercise

---
+++ C#/C# Advanced/C# OOP - June 2019/03.Encapsulation/Exercise/03.ShoppingSpree/Core/Engine.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShoppingSpree.Models;
+
+namespace ShoppingSpree.Core
+{
+    public class Engine
+    {
+        private List<Person> persons;
+        private List<Product> products;
+
+        public Engine()
+        {
+            persons = new List<Person>();
+            products = new List<Product>();
+        }
+
+        public void Run()
+        {
+            try
+            {
+                ReadPersonInfo();
+                ReadProductInfo();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            
+            string inputByParams = Console.ReadLine();
+
+            while (inputByParams.ToLower() != "end")
+            {
+                string[] buyCommand = inputByParams.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string personName = buyCommand[0];
+                string productName = buyCommand[1];
+
+                Product product = products.Where(p => p.Name == productName).FirstOrDefault();
+                Person person = persons.Where(p => p.Name == personName).FirstOrDefault();
+
+                try
+                {
+                    person.BuyProduct(product);
+                    Console.WriteLine($"{person.Name} bought {product.Name}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    Console.WriteLine(ioe.Message);
+                }
+
+                inputByParams = Console.ReadLine();
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine($"{person}");
+            }
+        }
+
+        private void ReadProductInfo()
+        {
+            products = Console.ReadLine()
+                                .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                                .ToList()
+                                .Select(l => l.Split('=', StringSplitOptions.RemoveEmptyEntries))
+                                .Select(p => new Product(p[0], decimal.Parse(p[1])))
+                                .ToList();
+        }
+
+        private void ReadPersonInfo()
+        {
+            persons = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries).ToList()
+                                .Select(t => t.Split('=', StringSplitOptions.RemoveEmptyEntries))
+                                .Select(p => new Person(p[0], decimal.Parse(p[1])))
+                                .ToList();
+        }
+    }
+}
--- C#/C# Advanced/C# OOP - June 2019/03.Encapsulation/Exercise/03.ShoppingSpree/StartUp.cs
+++ C#/C# Advanced/C# OOP - June 2019/03.Encapsulation/Exercise/03.ShoppingSpree/StartUp.cs
@@ -1,79 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using ShoppingSpree.Models;
-
-namespace ShoppingSpree
+﻿namespace ShoppingSpree
 {
+    using ShoppingSpree.Core;
+
     public class StartUp
     {
         static void Main()
         {
-            List<Person> persons = new List<Person>();
-
-            try
-            {
-                persons = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries).ToList()
-                    .Select(t => t.Split('=', StringSplitOptions.RemoveEmptyEntries))
-                    .Select(p => new Person(p[0], decimal.Parse(p[1])))
-                    .ToList();
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return;
-            }
-
-            List<Product> products = new List<Product>();
-
-            try
-            {
-                products = Console.ReadLine()
-                    .Split(";",StringSplitOptions.RemoveEmptyEntries)
-                    .ToList()
-                    .Select(l => l.Split('=', StringSplitOptions.RemoveEmptyEntries))
-                    .Select(p => new Product(p[0], decimal.Parse(p[1])))
-                    .ToList();
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return;
-            }
-
-
-            string inputByParams = Console.ReadLine();
-
-            while (inputByParams.ToLower() != "end")
-            {
-                string[] buyCommand = inputByParams.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                string personName = buyCommand[0];
-                string productName = buyCommand[1];
-
-                Product product = products.Where(p => p.Name == productName).FirstOrDefault();
-                Person person = persons.Where(p => p.Name == personName).FirstOrDefault();
-
-                try
-                {
-                    person.BuyProduct(product);
-                    Console.WriteLine($"{person.Name} bought {product.Name}");
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                catch (InvalidOperationException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-
-                inputByParams = Console.ReadLine();
-            }
-
-            foreach (var person in persons)
-            {
-                Console.WriteLine($"{person}");
-            }
+            Engine engine = new Engine();
+            engine.Run();
         }
     }
 }

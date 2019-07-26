1b74c3a9827d213bfc67d22bf135719368d721de
vladosfi <vladosfi@gmail.com>
2019-07-26 17:17:17 +0300

    OOP Reflection - Exercise

---
+++ C#/C# Advanced/C# OOP - June 2019/07.Reflection And Attributes/Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+
+        public string Read(string inpitLine)
+        {
+            string[] cmdTokens = inpitLine.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string commandName = cmdTokens[0] + COMMAND_POSTFIX;
+
+            string[] commandArgs = cmdTokens
+                .Skip(1)
+                .ToArray();
+
+            //Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Assembly assembly = Assembly
+                .GetCallingAssembly();
+
+
+            Type[] types = assembly
+                .GetTypes();
+
+            Type typeToCreate = types
+                .FirstOrDefault(t => t.Name == commandName);
+
+
+            Object instance = (ICommand)Activator
+                .CreateInstance(typeToCreate);
+
+            ICommand command = (ICommand)instance;
+
+            string result = command.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ C#/C# Advanced/C# OOP - June 2019/07.Reflection And Attributes/Exercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,29 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            string commandLine = Console.ReadLine();
+
+            while (commandLine != "Exit")
+            {
+                string result = this.commandInterpreter.Read(commandLine);
+                Console.WriteLine(result);
+
+                commandLine = Console.ReadLine();
+            }
+            
+        }
+    }
+}

0923a5c0230ab24fca37cc0d9fe222795f0ef86c
vladosfi <sfi@abv.bg>
2019-08-01 20:06:43 +0300

    C# OOP -Unit testing

--- C#/C# Advanced/C# OOP - June 2019/11.Unit-Testing/Exercise/Database.Tests/DatabaseTests.cs
+++ C#/C# Advanced/C# OOP - June 2019/11.Unit-Testing/Exercise/Database.Tests/DatabaseTests.cs
@@ -1,22 +1,35 @@
-using Database;
+using System;
 using NUnit.Framework;
+using DatabaseDemo;
 
 namespace Tests
 {
     [TestFixture]
     public class DatabaseTests
     {
+        private Database db;
+
         [SetUp]
         public void Setup()
         {
+            this.db = new Database(1, 2, 3, 4, 5, 6, 7, 8, 9);
         }
         
         [Test]
         public void CapacityMustBeExactly16Integers()
         {
-            Database.Database db = new Database.Database(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6);
+            //Assert.Throws<InvalidOperationException>(() => this.db.Count.Equals(16), "Array's capacity is not exactly 16 integers!");
+        }
 
-            Assert.That(db.Count, Not.EqualTo(16), "Dummy health doesn't change after attack.");
+        [Test]
+        public void AddMethodShoulThrowExceptionWithInvalidParameter()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                this.db.Add(i);
+            }
+            
+            Assert.Throws<InvalidOperationException>(() => this.db.Add(3));
         }
     }
 }

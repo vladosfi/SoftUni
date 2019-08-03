775a9ec38a547e2eb729b2e6dfd37dcaeca846e4
vladosfi <sfi@abv.bg>
2019-08-03 19:24:09 +0300

    C# OOP -Unit testing

--- C#/C# Advanced/C# OOP - June 2019/11.Unit-Testing/Exercise/FightingArena.Tests/ArenaTests.cs
+++ C#/C# Advanced/C# OOP - June 2019/11.Unit-Testing/Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,22 +1,94 @@
 ﻿using FightingArena;
 using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace Tests
 {
     public class ArenaTests
     {
         private Arena arena;
+        private Warrior warrior;
 
         [SetUp]
         public void Setup()
         {
-            arena = new Arena();
+            this.arena = new Arena();
+            this.warrior = new Warrior("Vapman", 50, 50);
+            this.arena.Enroll(warrior);
         }
 
         [Test]
-        public void Test1()
+        public void TestArenaConstructor()
         {
-            Assert.That(0,Is.EqualTo(arena.Count));
+            Assert.IsNotNull(this.arena.Warriors);
+        }
+
+        [Test]
+        public void TestEnrollingWorks()
+        {
+            Warrior expectedWarrior = this.arena.Warriors.Where(w => w.Name == "Vapman").FirstOrDefault();
+            Assert.That(warrior, Is.EqualTo(expectedWarrior));
+        }
+
+        [Test]
+        public void TestIfCounterWorks()
+        {
+            Assert.That(1, Is.EqualTo(arena.Count));
+        }
+
+        [Test]
+        public void AlreadyEnrolledWariorsShouldNotBeAbleToEnrollAgainException()
+        {
+            Warrior enrolledWarrior = new Warrior("Vapman", 28, 57);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Enroll(enrolledWarrior));
+        }
+
+        [Test]
+        public void WarriorCantFightIfIsNotEnrolledException()
+        {
+            Warrior notEnrolledWarrior = new Warrior("Pacman", 28, 57);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Fight(warrior.Name, notEnrolledWarrior.Name));
+        }
+
+        [Test]
+        public void WarirorAttakIfWariorDamageIsBiggerThanDefenderHp()
+        {
+            //Assert
+            Warrior defender = new Warrior("Pacman", 10, 50);
+            Warrior attacker = new Warrior("Crackman", 50, 50);
+            int expectedWariorHp = attacker.HP - defender.Damage;
+            int expectedFakeWariorHp = defender.HP - attacker.Damage;
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(defender);
+
+            //Act
+            this.arena.Fight(attacker.Name,defender.Name);
+
+            //Assert
+            Assert.That(expectedWariorHp, Is.EqualTo(attacker.HP));
+            Assert.That(expectedFakeWariorHp, Is.EqualTo(defender.HP));
+        }
+
+        [Test]
+        public void WarirorAttakIfWariorDamageIsSmallerThanDefenderHp()
+        {
+            //Assert
+            Warrior defender = new Warrior("Pacman", 50, 40);
+            Warrior attacker = new Warrior("Crackman", 50, 50);
+            int expectedAttackerHp = attacker.HP - defender.Damage;
+            int expectedDefenderHp = 0;
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(defender);
+
+            //Act
+            this.arena.Fight(attacker.Name, defender.Name);
+
+            //Assert
+            Assert.That(expectedAttackerHp, Is.EqualTo(attacker.HP));
+            Assert.That(expectedDefenderHp, Is.EqualTo(defender.HP));
         }
     }
 }
--- C#/C# Advanced/C# OOP - June 2019/11.Unit-Testing/Exercise/FightingArena.Tests/WarriorTests.cs
+++ C#/C# Advanced/C# OOP - June 2019/11.Unit-Testing/Exercise/FightingArena.Tests/WarriorTests.cs
@@ -6,38 +6,43 @@ namespace Tests
 {
     public class WarriorTests
     {
-        private Warrior warior;
         private readonly int defaultDamage = 50;
         private readonly int defaultHp = 50;
 
         [SetUp]
         public void Setup()
         {
-            warior = new Warrior("Vapman", defaultDamage, defaultHp);
         }
 
         [Test]
         public void TestConstructors()
         {
-            Assert.That("Vapman", Is.EqualTo(this.warior.Name));
-            Assert.That(defaultDamage, Is.EqualTo(this.warior.Damage));
-            Assert.That(defaultDamage, Is.EqualTo(this.warior.HP));
+            Warrior warrior = new Warrior("Vapman", defaultDamage, defaultHp);
+
+            Assert.AreEqual("Vapman", warrior.Name);
+            Assert.AreEqual(defaultDamage, warrior.Damage);
+            Assert.AreEqual(defaultHp, warrior.HP);
         }
 
 
         [Test]
-        public void TestWariorNameException()
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("         ")]
+        [TestCase(null)]
+        public void TestWariorNameException(string name)
         {
-            Assert.Throws<ArgumentException>(() => new Warrior(string.Empty, defaultDamage, defaultHp));
-            Assert.Throws<ArgumentException>(() => new Warrior(null, defaultDamage, defaultHp));
-            Assert.Throws<ArgumentException>(() => new Warrior(" ", defaultDamage, defaultHp));
+            Assert.Throws<ArgumentException>(() => new Warrior(name, 10, defaultHp));
         }
 
         [Test]
-        public void TestWariorDamageException()
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void TestWariorDamageException(int damage)
         {
-            Assert.Throws<ArgumentException>(() => new Warrior("Vapman", 0, defaultHp));
+            Assert.Throws<ArgumentException>(() => new Warrior("Vapman", damage, 10));
         }
+
 
         [Test]
         public void TestWariorHpException()
@@ -46,53 +51,60 @@ namespace Tests
         }
 
         [Test]
-        public void TestWariorHpMinAttakException()
+        public void TestWariorHpLowHp()
         {
-            Warrior fakeWarior = new Warrior("Pakman", defaultDamage, 30);
-            Assert.Throws<InvalidOperationException>(() => this.warior.Attack(fakeWarior));
+            Warrior attaker = new Warrior("Crackman", 10, 25);
+            Warrior defender = new Warrior("Pakman", 5, 45);
+            Assert.Throws<InvalidOperationException>(() => attaker.Attack(defender));
         }
 
         [Test]
-        public void TestFakeWariorHpMinAttakException()
+        public void TestAttackingEnemyWithLowHp()
         {
-            Warrior fakeWarior = new Warrior("Pakman", defaultDamage, 30);
-            this.warior = new Warrior("Vapman", defaultDamage, defaultHp);
-
-            Assert.Throws<InvalidOperationException>(() => this.warior.Attack(fakeWarior));
+            Warrior attaker = new Warrior("Crackman", 10, 45);
+            Warrior defender = new Warrior("Pakman", 5, 25);
+            Assert.Throws<InvalidOperationException>(() => attaker.Attack(defender));
         }
 
         [Test]
-        public void TestIfWarirorHpIsLowerThenFakeWariorDamageAttakException()
+        public void TestAttackingWithStrongerEnemy()
         {
-            Warrior fakeWarior = new Warrior("Pakman", 100, defaultHp);
-            this.warior = new Warrior("Vapman", defaultDamage, defaultHp);
+            Warrior attacker = new Warrior("Pakman", 10, 35);
+            Warrior deffender = new Warrior("Vapman", 40, 100);
 
-            Assert.Throws<InvalidOperationException>(() => this.warior.Attack(fakeWarior));
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(deffender));
         }
 
 
-        //Tri varianta na tozi test 
+
         [Test]
-        public void TestIfWarirorAttak()
+        public void Attack_WorkProperly()
         {
-            //Assert
-            Warrior fakeWarior = new Warrior("Pakman", 10, defaultHp);
-            this.warior = new Warrior("Vapman", defaultDamage, defaultHp);
-            int expectedWariorHp = this.warior.HP - fakeWarior.Damage;
-            int expectedFakeWariorHp = fakeWarior.HP - this.warior.Damage;
+            int expektedAttackerHp = 95;
+            int expektedDefenderrHp = 80;
 
-            if (expectedWariorHp < 0)
-            {
-                expectedFakeWariorHp = 0;
-            }
+            Warrior attacker = new Warrior("Vapman", 10, 100);
+            Warrior defender = new Warrior("Pacman", 5, 90);
 
-            //Act
-            this.warior.Attack(fakeWarior);
+            attacker.Attack(defender);
 
-            //Assert
-            Assert.That(expectedWariorHp, Is.EqualTo(this.warior.HP));
-            Assert.That(expectedFakeWariorHp, Is.EqualTo(fakeWarior.HP));
+            Assert.AreEqual(expektedAttackerHp, attacker.HP);
+            Assert.AreEqual(expektedDefenderrHp, defender.HP);
         }
 
+        [Test]
+        public void TestKillingEnemy()
+        {
+            int expektedAttackerHp = 55;
+            int expektedDefenderrHp = 0;
+
+            Warrior attacker = new Warrior("Vapman", 50, 100);
+            Warrior defender = new Warrior("Pacman", 45, 40);
+
+            attacker.Attack(defender);
+
+            Assert.AreEqual(expektedAttackerHp, attacker.HP);
+            Assert.AreEqual(expektedDefenderrHp, defender.HP);
+        }
     }
 }

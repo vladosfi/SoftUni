c5a13513f5286a8901dd165fac4036ab5811f325
vladosfi <vladosfi@gmail.com>
2019-09-27 16:46:09 +0300

    DB - Exercises

--- C#/Databases Basics - MS SQL Server/Old - Course/06.DB-Basics-MSSQL-Table-Relations-Exercises.sql
+++ C#/Databases Basics - MS SQL Server/Old - Course/06.DB-Basics-MSSQL-Table-Relations-Exercises.sql
@@ -33,4 +33,82 @@ INSERT INTO Persons(FirstName,Salary,PassportID)
 --Problem 2.	One-To-Many Relationship
 --Create two tables as follows. Use appropriate data types.
 
+CREATE TABLE Manufacturers(
+ManufacturerID INT PRIMARY KEY,
+[Name] VARCHAR(20) UNIQUE,	
+EstablishedOn SMALLDATETIME
+)
+
+CREATE TABLE Models(
+ModelID	INT PRIMARY KEY,
+[Name] VARCHAR(20) UNIQUE,	
+ManufacturerID INT
+CONSTRAINT FK_Peaks_Mountains
+FOREIGN KEY (ManufacturerID)
+REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Manufacturers(ManufacturerID,[Name],EstablishedOn)
+	VALUES(1,'BMW','07/03/1916'),
+	(2,'Tesla','01/01/2003'),
+	(3,'Lada','01/05/1966')
+
+INSERT INTO Models(ModelID,[Name],ManufacturerID)
+	VALUES(101,'X1',1),
+	(102,'i6',1),
+	(103,'Model S',2),
+	(104,'Model X',2),
+	(105,'Model 3',2),
+	(106,'Nova',3)
+
+
+--Problem 3.	Many-To-Many Relationship
+--Create three tables as follows. Use appropriate data types.
+
+CREATE TABLE Students(
+StudentID INT PRIMARY KEY,
+[Name] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Exams(
+ExamID INT PRIMARY KEY,
+[Name] VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE StudentsExams(
+StudentID INT NOT NULL,
+ExamID INT NOT NULL
+)
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT PK_StudentsExams PRIMARY KEY (StudentID,ExamID)
+
+
+ALTER TABLE StudentsExams
+ADD FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
+
+ALTER TABLE StudentsExams
+ADD FOREIGN KEY (ExamID) REFERENCES Exams(ExamID)
+
+
+INSERT INTO Students(StudentID,[Name])
+	VALUES(1,'Mila'),
+	(2,'Toni'),
+	(3,'Ron')
+
+
+INSERT INTO Exams(ExamID,[Name])
+	VALUES(101,'SpringMVC'),
+	(102,'Neo4j'),
+	(103,'Oracle 11g')
+
+INSERT INTO StudentsExams(StudentID, ExamID)
+	VALUES(1,101),
+	(1,102),
+	(2,101),
+	(3,103),
+	(2,102),
+	(2,103)
+
+	
 

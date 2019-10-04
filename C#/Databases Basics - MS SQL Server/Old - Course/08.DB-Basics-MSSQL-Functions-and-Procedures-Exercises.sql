fa732f5c8efb9e074814b2b6e6883cc7884d13f9
vladosfi <vladosfi@gmail.com>
2019-10-04 09:36:31 +0300

    DB - Exercises

--- C#/Databases Basics - MS SQL Server/Old - Course/08.DB-Basics-MSSQL-Functions-and-Procedures-Exercises.sql
+++ C#/Databases Basics - MS SQL Server/Old - Course/08.DB-Basics-MSSQL-Functions-and-Procedures-Exercises.sql
@@ -41,3 +41,147 @@ EXEC usp_GetTownsStartingWith 'b'
 
 --Problem 4.	Employees from Town
 --Write a stored procedure usp_GetEmployeesFromTown that accepts town name as parameter and return the employees’ first and last name that live in the given town. 
+GO
+	CREATE PROC usp_GetEmployeesFromTown(@TownName VARCHAR (50))
+AS
+	SELECT FirstName, LastName
+		FROM Employees AS e
+		JOIN Addresses AS a
+		ON e.AddressID = a.AddressID
+		JOIN Towns AS t
+		ON a.TownID = t.TownID
+	WHERE t.[Name] = @TownName 
+GO
+
+EXEC usp_GetEmployeesFromTown 'Sofia'
+
+
+--Problem 5.	Salary Level Function
+--Write a function ufn_GetSalaryLevel(@salary DECIMAL(18,4)) that receives salary of an employee and returns the level of the salary.
+--•	If salary is < 30000 return “Low”
+--•	If salary is between 30000 and 50000 (inclusive) return “Average”
+--•	If salary is > 50000 return “High”
+
+
+CREATE OR ALTER FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4)) 
+RETURNS VARCHAR(10) 
+BEGIN
+DECLARE @result AS CHAR(10)
+IF (@salary < 30000 )
+BEGIN
+	SET @result = 'Low'
+END
+ELSE IF (@salary <= 50000)
+BEGIN
+	SET @result = 'Average'
+END
+ELSE
+BEGIN
+	SET @result = 'High'
+END		
+RETURN @result
+END
+
+SELECT dbo.ufn_GetSalaryLevel(60000) AS [Salary Level]
+
+
+--Problem 6.	Employees by Salary Level
+--Write a stored procedure usp_EmployeesBySalaryLevel that receive as parameter level of salary (low, average or high) and print the names of all employees that have given level of salary. You should use the function - “dbo.ufn_GetSalaryLevel(@Salary)”, which was part of the previous task, inside your “CREATE PROCEDURE …” query.
+GO
+CREATE PROC usp_EmployeesBySalaryLevel(@LevelOfSalary CHAR(10))
+AS
+	SELECT FirstName, LastName 
+	FROM Employees e
+	WHERE (SELECT dbo.ufn_GetSalaryLevel(e.Salary)) =  @LevelOfSalary
+GO
+
+EXEC usp_EmployeesBySalaryLevel 'high'
+
+--Problem 7.	Define Function
+--Define a function ufn_IsWordComprised(@setOfLetters, @word) that returns true or false depending on that if the word is a comprised of the given set of letters. 
+CREATE OR ALTER FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR(MAX), @word VARCHAR(MAX))
+RETURNS BIT
+BEGIN
+	DECLARE @Count INT = 1
+	WHILE(@Count <= LEN(@word))
+	BEGIN
+		IF(CHARINDEX(SUBSTRING(@word, @Count, 1), @setOfLetters) = 0)
+		BEGIN		
+			RETURN 0
+		END
+		ELSE
+		BEGIN
+			SET @Count += 1
+		END
+	END
+	RETURN 1
+END 
+
+SELECT dbo.ufn_IsWordComprised('oistmiahf', 'halves')
+
+
+
+--Problem 8.	* Delete Employees and Departments
+--Write a procedure with the name usp_DeleteEmployeesFromDepartment (@departmentId INT) which deletes all Employees from a given department. Delete these departments from the Departments table too. Finally SELECT the number of employees from the given department. If the delete statements are correct the select query should return 0.
+--After completing that exercise restore your database to revert all changes.
+
+-- alter table departments set ManagerId column null
+-- delete from EmployeesProjects
+-- Update department set managerId column = null
+-- delete from employees where depId = dep
+-- delete from departments where id = @id
+
+
+CREATE OR ALTER PROC usp_DeleteEmployeesFromDepartment (@departmentId INT) 
+AS
+ALTER TABLE Departments
+ALTER COLUMN ManagerID INT 
+
+DELETE FROM EmployeesProjects 
+WHERE EmployeeID IN (SELECT EmployeeID FROM Employees WHERE DepartmentID = @departmentId)
+
+UPDATE Departments 
+SET ManagerID = NULL
+WHERE DepartmentID = @departmentId
+
+UPDATE Employees
+SET ManagerID = NULL
+WHERE ManagerID IN (SELECT EmployeeID FROM Employees WHERE DepartmentID = @departmentId)
+
+DELETE FROM Employees
+WHERE DepartmentID = @departmentId
+
+DELETE FROM Departments
+WHERE DepartmentID = @departmentId
+SELECT COUNT(*) FROM Employees	 WHERE DepartmentID = @departmentId
+
+EXEC usp_DeleteEmployeesFromDepartment 1
+	
+
+--	Problem 9.	Find Full Name
+--You are given a database schema with tables AccountHolders(Id (PK), FirstName, LastName, SSN) and Accounts(Id (PK), AccountHolderId (FK), Balance).  Write a stored procedure usp_GetHoldersFullName that selects the full names of all people. 
+CREATE PROC usp_GetHoldersFullName 
+AS
+SELECT CONCAT(FirstName,' ', LastName) AS [Full Name]  FROM AccountHolders
+
+EXEC usp_GetHoldersFullName 
+
+--Problem 10.	People with Balance Higher Than
+--Your task is to create a stored procedure usp_GetHoldersWithBalanceHigherThan that accepts a number as a parameter and returns all people who have more money in total of all their accounts than the supplied number. 
+CREATE OR ALTER PROC usp_GetHoldersWithBalanceHigherThan (@Number MONEY)
+AS
+SELECT s.FirstName, s.LastName FROM(
+	SELECT ah.FirstName, ah.LastName, ah.Id
+	FROM AccountHolders AS ah
+		JOIN Accounts AS a
+		ON ah.Id = a.AccountHolderId
+	GROUP BY ah.FirstName, ah.LastName, ah.Id
+	HAVING SUM(a.Balance) > @Number) as s
+ORDER BY s.FirstName, s.LastName
+
+EXEC usp_GetHoldersWithBalanceHigherThan 50000
+
+
+
+
+

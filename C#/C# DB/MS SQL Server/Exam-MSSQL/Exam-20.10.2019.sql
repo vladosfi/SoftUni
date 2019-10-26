ef0e21da4b065e15a6392d117d6a835a87de8559
vladosfi <sfi@abv.bg>
2019-10-26 10:12:45 +0300

    C# DB - Entity Framework Core - октомври 2019

--- C#/C# DB/MS SQL Server/Exam-MSSQL/Exam-20.10.2019.sql
+++ C#/C# DB/MS SQL Server/Exam-MSSQL/Exam-20.10.2019.sql
@@ -148,21 +148,21 @@ SELECT
 	ISNULL(e.FirstName + ' ' + e.LastName, 'None') AS Employee,
 	ISNULL(d.Name, 'None') AS Department,
 	ISNULL(c.Name, 'None') AS Category,
-	ISNULL(r.Description, 'None') AS Description,
+	ISNULL(r.Description, 'None') AS [Description],
 	ISNULL(FORMAT(r.OpenDate,'dd.MM.yyyy'), 'None') AS OpenDate,
-	ISNULL(s.Label, 'None') AS Status,
+	ISNULL(s.Label, 'None') AS [Status],
 	ISNULL(u.Name, 'None') AS [User]
 	FROM Reports AS r
 	LEFT JOIN Employees AS e
 	ON e.Id = r.EmployeeId
+	LEFT JOIN Departments AS d
+	ON d.Id = e.DepartmentId
 	LEFT JOIN Categories AS c
-	ON c.DepartmentId = r.CategoryId
+	ON c.Id = r.CategoryId
 	LEFT JOIN [Status] AS s
 	ON s.Id = r.StatusId	
 	LEFT JOIN Users AS u
 	ON u.Id = r.UserId	
-	JOIN Departments AS d
-	ON d.Id = e.DepartmentId
 ORDER BY 
 	FirstName DESC,
 	LastName DESC,

1f15bd5faea9ec445e3005a47f072c6d53473b0b
vladosfi <sfi@abv.bg>
2019-10-30 21:05:39 +0200

    C# DB Advanced - Entity Framework - 02.ORM-Fundamentals-MiniORM-Exercise

--- C#/DB/Entity Framework Core/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbContext.cs
+++ C#/DB/Entity Framework Core/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbContext.cs
@@ -1,4 +1,22 @@
-﻿namespace MiniORM
+﻿using System;
+
+namespace MiniORM
 {
 	// TODO: Create your DbContext class here.
+
+    public class DbContext
+    {
+        public static Type[] AllowedSqlTypes =
+        {
+            typeof(bool),
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(DateTime),
+            typeof(double),
+            typeof(decimal),
+            typeof(long),
+            typeof(ulong)
+        };
+    }
 }
--- C#/DB/Entity Framework Core/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbSet.cs
+++ C#/DB/Entity Framework Core/02.ORM-Fundamentals-MiniORM-Exercise/MiniORM/DbSet.cs
@@ -1,4 +1,12 @@
-﻿namespace MiniORM
+﻿using System.Collections.Generic;
+
+namespace MiniORM
 {
 	// TODO: Create your DbSet class here.
+
+    public class DbSet<T>
+    {
+        public List<T> Entities { get; set; }
+    }
+
 }

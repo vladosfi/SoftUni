dbc52b1324f18308580717156d3c7c60469127c6
vladosfi <sfi@abv.bg>
2019-11-02 19:00:39 +0200

    C# DB Advanced - Entity Framework -03.Introduction-to-EntityFramework-Exercise

---
+++ C#/DB/Entity Framework Core/03.Introduction-to-EntityFramework/DatabaseFirst/StartUp.cs
@@ -0,0 +1,141 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using SoftUni.Models;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            string result;
+            
+            //result = GetEmployeesFullInformation(context);
+            //Console.WriteLine(result);
+
+            //result = GetEmployeesWithSalaryOver50000(context);
+            //Console.WriteLine(result);
+
+            //result = GetEmployeesFromResearchAndDevelopment(context);
+            //Console.WriteLine(result);
+
+            result = AddNewAddressToEmployee(context);
+            Console.WriteLine(result);
+
+        }
+
+        //06.Adding a New Address and Updating Employee
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Address address = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            context.Addresses.Add(address);
+            
+            Employee nakov = context
+                .Employees
+                .First(e => e.LastName == "Nakov");
+
+            nakov.Address = address;
+
+            context.SaveChanges();
+
+            var employees = context
+                .Employees
+                .OrderByDescending(e=>e.AddressId)
+                .Take(10)
+                .Select(e => new
+                {
+                    e.Address
+                });
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.Address.AddressText}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //05.Employees from Research and Development
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Salary
+                });
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from Research and Development - ${employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //04.Employees with Salary Over 50 000
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000);
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //03.Employees Full Information
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    Id = e.EmployeeId,
+                    Name = string.Join(" ", e.FirstName, e.LastName, e.MiddleName),
+                    e.JobTitle,
+                    e.Salary
+                });
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.Name} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

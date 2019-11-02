13229c81dc215583e649380b51f1833f1e80819d
vladosfi <sfi@abv.bg>
2019-11-02 12:08:28 +0200

    C# DB Advanced - Entity Framework -03.Introduction-to-EntityFramework-Exercise

---
+++ C#/DB/Entity Framework Core/03.Introduction-to-EntityFramework/DatabaseFirst/02.DatabaseFirst/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    public class StartUp
+    {
+        static void Main()
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            //string result = GetEmployeesFullInformation(context);
+
+            Console.WriteLine(result);
+        }
+
+        // 03.Employees Full Information
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    Id = e.EmployeeId,
+                    Name = string.Join(" ", e.FirstName, e.LastName, e.MiddleName),
+                    e.JobTitle,
+                    e.Salary
+                });
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.Name} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

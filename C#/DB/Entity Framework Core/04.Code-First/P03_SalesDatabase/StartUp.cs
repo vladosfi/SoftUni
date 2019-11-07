0e5640a21d09ee7d36f8b85b267234c3e5c1d970
vladosfi <sfi@abv.bg>
2019-11-07 20:49:56 +0200

    C# DB Advanced - Entity Framework -04.Code First - Complete

--- C#/DB/Entity Framework Core/04.Code-First/P03_SalesDatabase/StartUp.cs
+++ C#/DB/Entity Framework Core/04.Code-First/P03_SalesDatabase/StartUp.cs
@@ -1,12 +1,55 @@
 ﻿namespace P03_SalesDatabase
 {
+    using System.Collections.Generic;
     using Data;
+    using P03_SalesDatabase.Data.Models;
 
     public class StartUp
     {
         public static void Main()
         {
-            var db = new SalesContext();
+            using(var db = new SalesContext())
+            {
+                var productsToAdd = GetProductsToSeed();
+
+                db.Products.AddRange(productsToAdd);
+
+                db.SaveChanges();
+            }
+        }
+
+        private static List<Product> GetProductsToSeed()
+        {
+            List<Product> products = new List<Product>();
+
+            Product product1 = new Product
+            {
+                Name = "Lemon",
+                Quantity = 10,
+                Price = 2.50m
+            };
+
+            Product product2 = new Product
+            {
+                Name = "Tomatoes",
+                Quantity = 20,
+                Price = 4.50m
+            };
+
+            Product product3 = new Product
+            {
+                Name = "Aple",
+                Quantity = 15,
+                Price = 1.50m
+            };
+
+            products.Add(product1);
+
+            products.Add(product2);
+
+            products.Add(product3);
+
+            return products;
         }
     }
 }

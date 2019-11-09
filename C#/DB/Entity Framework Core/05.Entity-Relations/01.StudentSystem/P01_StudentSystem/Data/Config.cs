b643d0a3b7b2a2b3f6350320a0d12ec546e46c0d
vladosfi <sfi@abv.bg>
2019-11-09 20:32:49 +0200

    C# DB Advanced - Entity Framework -05.Entity-Relations

---
+++ C#/DB/Entity Framework Core/05.Entity-Relations/01.StudentSystem/P01_StudentSystem/Data/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_StudentSystem.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = "Server=.\\SQLEXPRESS;Database=StudentSystem;Integrated Security=true;";
+    }
+}
---
+++ C#/DB/Entity Framework Core/05.Entity-Relations/01.StudentSystem/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,57 @@
+﻿namespace P01_StudentSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using P01_StudentSystem.Data.Models;
+
+    public class StudentSystemContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Resource> Resource { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<StudentCourse> CourseEnrollments { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnConfiguringStuden(modelBuilder);
+
+        }
+
+        private static void OnConfiguringStuden(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Student>(entity =>
+                {
+                    entity.HasKey(s => s.StudentId);
+
+                    entity
+                        .Property(p => p.Name)
+                        .HasMaxLength(100)
+                        .IsUnicode()
+                        .IsRequired();
+
+                    entity
+                        .Property(s => s.PhoneNumber)
+                        .HasColumnType("CHAR(10)") //.HasMaxLength(10).IsFixedLength()
+                        .IsUnicode(false);
+
+                    entity
+                        .Property(s => s.RegisteredOn)
+                        .HasDefaultValueSql("GETDATE()");
+                });
+        }
+    }
+}

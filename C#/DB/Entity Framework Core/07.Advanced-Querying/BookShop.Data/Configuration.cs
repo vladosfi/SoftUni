0973ca8f61b36dc6aa6ce8cc3bfcf89ebf5ae4e3
vladosfi <vladosfi@gmail.com>
2019-11-12 17:32:51 +0200

    DB EF - Database - Advanced-Querying

---
+++ C#/DB/Entity Framework Core/07.Advanced-Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        //internal static string ConnectionString => "Server=.;Database=BookShop;Integrated Security=True;";
+        internal static string ConnectionString => "Server=.\\SQLEXPRESS;Database=BookShop;Integrated Security=true;";
+
+    }
+}
---
+++ C#/DB/Entity Framework Core/07.Advanced-Querying/BookShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using BookShop.Models.Enums;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            using (var db = new BookShopContext())
+            {  
+
+                //DbInitializer.ResetDatabase(db);
+                
+                Console.WriteLine(GetBooksByAgeRestriction(db, Console.ReadLine()));
+            }
+        }
+
+        //1.	Age Restriction
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            AgeRestriction ageRestriction = (AgeRestriction)Enum.Parse(typeof(AgeRestriction), command,true);
+
+            var bookTitles = context
+                .Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .OrderBy(b=>b.Title)
+                .Select(b => b.Title)
+                .ToList();
+
+
+            foreach (var book in bookTitles)
+            {
+                sb.AppendLine(book);
+            }
+
+            return sb.ToString().Trim();   
+        }
+    }
+}

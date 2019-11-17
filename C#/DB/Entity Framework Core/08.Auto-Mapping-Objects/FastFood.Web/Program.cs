9535198374759823d37877b0e76b18c64e56163b
vladosfi <sfi@abv.bg>
2019-11-17 18:06:22 +0200

    C# DB Advanced - Entity Framework - 11. DB-Advanced-JSON-Processing

--- C#/DB/Entity Framework Core/08.Auto-Mapping-Objects/FastFood.Web/Program.cs
+++ C#/DB/Entity Framework Core/08.Auto-Mapping-Objects/FastFood.Web/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,6 +15,8 @@ namespace FastFood.Web
     {
         public static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
             CreateWebHostBuilder(args).Build().Run();
         }
 

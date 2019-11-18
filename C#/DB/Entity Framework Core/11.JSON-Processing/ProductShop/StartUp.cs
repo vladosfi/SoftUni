daf3b4d37d8e16ec82b0d73019d9e9fc305f97f6
vladosfi <vladosfi@gmail.com>
2019-11-18 17:53:22 +0200

    DB EF - Database - 11.JSON-Processing

---
+++ C#/DB/Entity Framework Core/11.JSON-Processing/ProductShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace ProductShop
+{
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+    using System.IO;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+
+            var usersJson = File.ReadAllText("./../../../Datasets/users.json");
+
+            System.Console.WriteLine(usersJson);
+            var results = ImportUsers(context, usersJson);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson)
+                .Where(u => u.LastName != null && u.LastName.Length >= 3)
+                .ToArray();
+
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}"; ;
+        }
+    }
+}

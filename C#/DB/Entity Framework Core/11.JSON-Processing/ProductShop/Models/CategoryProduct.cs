a915bdfaae0f868ad135fe9066631b314e7792d2
vladosfi <vladosfi@gmail.com>
2019-11-19 17:34:58 +0200

    DB EF - Database - 11.JSON-Processing

--- C#/DB/Entity Framework Core/11.JSON-Processing/ProductShop/Models/CategoryProduct.cs
+++ C#/DB/Entity Framework Core/11.JSON-Processing/ProductShop/Models/CategoryProduct.cs
@@ -7,5 +7,23 @@
 
         public int ProductId { get; set; }
         public Product Product { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = (CategoryProduct) obj;
+
+            return this.CategoryId == other.CategoryId &&
+                this.ProductId == other.ProductId;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+
+            hash = (hash * 7) + CategoryId.GetHashCode();
+            hash = (hash * 7) + ProductId.GetHashCode();
+
+            return hash;
+        }
     }
 }
--- C#/DB/Entity Framework Core/11.JSON-Processing/ProductShop/StartUp.cs
+++ C#/DB/Entity Framework Core/11.JSON-Processing/ProductShop/StartUp.cs
@@ -1,9 +1,12 @@
 ﻿namespace ProductShop
 {
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
     using ProductShop.Data;
+    using ProductShop.Dtos.Export;
     using ProductShop.Models;
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
 
@@ -25,20 +28,112 @@
             //Console.WriteLine(ImportCategories(context, categoriesJson));
 
 
-            var categoriesProductsJson = File.ReadAllText("./../../../Datasets/categories-products.json");
-            Console.WriteLine(ImportCategoryProducts(context, categoriesProductsJson));
+            //var categoriesProductsJson = File.ReadAllText("./../../../Datasets/categories-products.json");
+            //Console.WriteLine(ImportCategoryProducts(context, categoriesProductsJson));
+
+            //Console.WriteLine(GetProductsInRange(context));
+            Console.WriteLine(GetSoldProducts(context));
         }
+
+        //Query 6. Export Successfully Sold Products
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var filteredUsers = context
+                .Users
+                .Where(u=>u.ProductsSold.Any(ps => ps.Buyer!= null))
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Select(u => 
+                    new 
+                    {
+                        FirstName = u.FirstName,
+                        LastName = u.LastName,
+                        SoldProducts = u.ProductsSold
+                            .Select(ps => new 
+                            {
+                                Name = ps.Name,
+                                Price = ps.Price,
+                                BuyerFirstName = ps.Buyer.FirstName,
+                                BuyerLastName = ps.Buyer.LastName,
+                            })
+                          .ToArray()  
+                    })
+                .ToArray();
+
+            DefaultContractResolver contractResolver = new DefaultContractResolver()
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            var json = JsonConvert.SerializeObject(filteredUsers, new JsonSerializerSettings()
+            {
+                ContractResolver = contractResolver,
+                Formatting = Formatting.Indented
+            });
+
+            return json;
+        }
+
+        //Query 5. Export Products In Range
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var productsInRange = context
+                .Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .Select(p => new ProductDto
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    Seller = $"{p.Seller.FirstName} {p.Seller.LastName}"
+                })
+                .OrderBy(p => p.Price)
+                .ToList();
+
+
+            var json = JsonConvert.SerializeObject(productsInRange, Formatting.Indented);
+
+            return string.Join(Environment.NewLine, json);
+
+        }
+
 
         //Query 4. Import Categories and Products
         public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
         {
+            var validProdictId = new HashSet<int>
+            (
+                context
+                .CategoryProducts
+                .Select(p => p.ProductId));
+
+            var validCategoryId = new HashSet<int>
+            (
+                context
+                .CategoryProducts
+                .Select(p => p.CategoryId));
+
             var categoriesProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson)
+                .Distinct()
                 .ToArray();
 
-            context.AddRange(categoriesProducts);
+            var validCategoriesProducts = new List<CategoryProduct>();
+
+            foreach (var categoryProduct in categoriesProducts)
+            {
+                var isValid = validProdictId.Contains(categoryProduct.ProductId) &&
+                                validCategoryId.Contains(categoryProduct.CategoryId);
+
+                if (isValid)
+                {
+                    validCategoriesProducts.Add(categoryProduct);
+                }
+            }
+
+
+            context.AddRange(validCategoriesProducts);
             context.SaveChanges();
 
-            return $"Successfully imported {categoriesProducts.Length}";
+            return $"Successfully imported {validCategoriesProducts.Count}";
         }
 
         //Query 3. Import Categories

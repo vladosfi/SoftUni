2500ef86b62c9ff102280c8bd817553e73932fde
vladosfi <vladosfi@gmail.com>
2019-11-28 17:32:27 +0200

    DB EF - Database - 11.Design-Patterns

---
+++ C#/DB/Entity Framework Core/11.Design-Patterns/01.Prototype-Patterrn/Prototype/Prototype/SandwichPrototype.cs
@@ -0,0 +1,7 @@
+﻿namespace Prototype.Prototype
+{
+    public abstract class SandwichPrototype
+    {
+        public abstract SandwichPrototype Clone();
+    }
+}
---
+++ C#/DB/Entity Framework Core/11.Design-Patterns/01.Prototype-Patterrn/Prototype/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace Prototype
+{
+    using Prototype;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            SandwichMenu sandwichMenu = new SandwichMenu();
+
+            //Initialize with default sandwiches
+            sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut Butter, Jelly");
+            sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+            //Custom Sandwiches
+            sandwichMenu["LoadedBLT"] = new Sandwich("Wheat","Turkey, Bacon", "American", "Lettuce, Tomato, Onion, Olives");
+            sandwichMenu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Lettuce, Onion");
+            sandwichMenu["Vegetarian"] = new Sandwich("Wheat", "", "", "Lettuce, Onion, Tomato, Olives, Spinach");
+
+            //Clone these sandwiches
+
+        }
+    }
+}
+ 

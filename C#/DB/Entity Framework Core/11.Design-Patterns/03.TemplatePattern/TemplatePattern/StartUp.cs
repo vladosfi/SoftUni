2f7e2f265d34b4a090d7f5634754e7b7d32916ad
vladosfi <sfi@abv.bg>
2019-11-28 19:59:17 +0200

    C# DB Advanced - Entity Framework - 11.Design-Patterns

---
+++ C#/DB/Entity Framework Core/11.Design-Patterns/03.TemplatePattern/TemplatePattern/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace TemplatePattern
+{
+    public class StartUp
+    {
+        private static void Main()
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}

29b4b75fd869e38c40b57d73196f55ff09640199
vladosfi <sfi@abv.bg>
2019-12-02 11:01:46 +0200

    C# DB Advanced - Entity Framework - Exam prep 1 - 100%

---
+++ C#/DB/Entity Framework Core/ExamPrep/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/DataProcessor/ExportDtos/ExportSongsDto.cs
@@ -0,0 +1,33 @@
+﻿
+namespace MusicHub.DataProcessor.ExportDtos
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Song")]
+    public class ExportSongsDto
+    {
+        [XmlElement("SongName")]
+        public string SongName { get; set; }
+
+        [XmlElement("Writer")]
+        public string Writer { get; set; }
+
+        [XmlElement("Performer")]
+        public string Performer { get; set; }
+
+        [XmlElement("AlbumProducer")]
+        public string AlbumProducer { get; set; }
+
+        [XmlElement("Duration")]
+        public string Duration { get; set; }
+
+        //            <Song>
+        //  <SongName>Away</SongName>
+        //  <Writer>Norina Renihan</Writer>
+        //  <Performer>Lula Zuan</Performer>
+        //  <AlbumProducer>Georgi Milkov</AlbumProducer>
+        //  <Duration>00:05:35</Duration>
+        //</Song>
+
+    }
+}
--- C#/DB/Entity Framework Core/ExamPrep/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/DataProcessor/Serializer.cs
+++ C#/DB/Entity Framework Core/ExamPrep/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/DataProcessor/Serializer.cs
@@ -1,19 +1,78 @@
 ﻿namespace MusicHub.DataProcessor
 {
     using System;
-
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
     using Data;
+    using MusicHub.DataProcessor.ExportDtos;
+    using Newtonsoft.Json;
 
     public class Serializer
     {
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            throw new NotImplementedException();
+            var albums = context
+                .Albums
+                .Where(a => a.ProducerId == producerId)
+                .OrderByDescending(a => a.Price)
+                .Select(a => new
+                {
+                    AlbumName = a.Name,
+                    ReleaseDate = a.ReleaseDate.ToString(@"MM/dd/yyyy"),
+                    ProducerName = a.Producer.Name,
+                    Songs = a.Songs
+                        .Select(s => new
+                        {
+                            SongName = s.Name,
+                            Price = string.Format($"{s.Price:f2}"),
+                            Writer = s.Writer.Name
+                        })
+                        .OrderByDescending(s => s.SongName)
+                        .ThenBy(s => s.Writer)
+                        .ToList(),
+                    AlbumPrice = string.Format($"{a.Price:f2}")
+                })
+                .ToList();
+
+
+            var json = JsonConvert.SerializeObject(albums, Newtonsoft.Json.Formatting.Indented);
+
+            return string.Join(Environment.NewLine, json);
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            var products = context
+                .Songs
+                .Where(s => s.Duration.TotalSeconds > duration)
+                .Select(s => new ExportSongsDto
+                {
+                    SongName = s.Name,
+                    Writer = s.Writer.Name,
+                    Performer = s.SongPerformers
+                        .Select(p=>$"{p.Performer.FirstName} {p.Performer.LastName}").FirstOrDefault(),
+                    AlbumProducer = s.Album.Producer.Name,
+                    Duration = s.Duration.ToString(@"hh\:mm\:ss")
+                })
+                .OrderBy(s=>s.SongName)
+                .ThenBy(s=>s.Writer)
+                .ThenBy(s=>s.Performer)
+                .ToArray();
+
+            //Serialize XML
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportSongsDto[]), new XmlRootAttribute("Songs"));
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+
+            serializer.Serialize(new StringWriter(sb), products, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }

f9c93597ee576fb54f5be0a53b11dda3b9f74bc1
vladosfi <sfi@abv.bg>
2019-12-12 10:39:36 +0200

    C# DB Advanced - Entity Framework - ExamPrep

--- C#/DB/Entity Framework Core/ExamPrep/C# DB Advanced Retake Exam - 05.01.2018/PetClinic/DataProcessor/Bonus.cs
+++ C#/DB/Entity Framework Core/ExamPrep/C# DB Advanced Retake Exam - 05.01.2018/PetClinic/DataProcessor/Bonus.cs
@@ -1,14 +1,29 @@
 ﻿namespace PetClinic.DataProcessor
 {
     using System;
-
+    using System.Linq;
     using PetClinic.Data;
 
     public class Bonus
     {
         public static string UpdateVetProfession(PetClinicContext context, string phoneNumber, string newProfession)
         {
-            throw new NotImplementedException();
+            var vet = context.Vets.Where(v => v.PhoneNumber == phoneNumber).FirstOrDefault();
+
+            if (vet == null)
+            {
+                return $"Vet with phone number {phoneNumber} not found!";
+            }
+            else
+            {
+                var oldProfession = vet.Profession;
+
+                vet.Profession = newProfession;
+                context.Vets.Update(vet);
+                context.SaveChanges();
+
+                return $"{vet.Name}'s profession updated from {oldProfession} to {newProfession}.";
+            }
         }
     }
 }

2346bfb91537d5df3baa69baf61a48c5363b3ba6
vladosfi <vladosfi@gmail.com>
2020-01-03 17:07:28 +0200

    DB EF - Design Patterns

---
+++ Design-Patterns/DP/DP/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace Prototype
+{
+    using System;
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu sandwichMenu = new SandwichMenu();
+
+            //Initialize with default sandwiches
+            sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut butter, Jelly");
+            sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+            //Deli manager adds custom sandwiches
+            sandwichMenu["LoadedBLT"] = new Sandwich("Wheat", "Turkey, Bacon", "American", "Lettuce, Tomato, Onion, Olives");
+            sandwichMenu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Lettuce, Onion");
+            sandwichMenu["Vegetarian"] = new Sandwich("Wheat", "", "", "Lettuce, Tomato, Onion, Olives, Spinach");
+
+
+            //Clone this sandwiches
+            Sandwich sandwich1 = sandwichMenu["BLT"].Clone() as Sandwich;
+            Sandwich sandwich2 = sandwichMenu["ThreeMeatCombo"].Clone() as Sandwich;
+            Sandwich sandwich3 = sandwichMenu["Vegetarian"].Clone() as Sandwich;
+
+
+
+        }
+    }
+}

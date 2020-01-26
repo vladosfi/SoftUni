6d270adcf42833b2db3afd205feb2ee747e0aad1
vladosfi <sfi@abv.bg>
2020-01-26 19:17:52 +0200

    C# WEB

---
+++ C#/C# WEB/AsyncAwait/AsinchronusTask/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsinchronusTask
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Console.WriteLine("Starting");
+
+            var worker = new Worker();
+            worker.DoWork();
+
+            while (!worker.IsComplete)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+
+            Console.WriteLine("Done");
+        }
+    }
+
+    internal class Worker
+    {
+        public bool IsComplete { get; private set; }
+
+        public async void DoWork()
+        {
+            IsComplete = false;
+            Console.WriteLine("Doing work");
+
+            await LongOperation();
+
+            Console.WriteLine("Work completed");
+            IsComplete = true;
+        }        
+
+        private Task LongOperation()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Working");
+                Thread.Sleep(2000);
+            });
+        }
+    }
+}
---
+++ C#/C# WEB/AsyncAwait/AsyncDemo/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    class Startup
+    {
+        static void Main()
+        {
+            
+            var number = Task.Run(() => NumberOfPrimesInInterval(2, 100000));
+            number.ContinueWith((task) => Console.WriteLine(task.Result + " comes from thread #" + Thread.CurrentThread.ManagedThreadId));
+            
+            //Wait task to complete
+            //number.Wait();
+
+
+            Console.WriteLine("Main thread #"+ Thread.CurrentThread.ManagedThreadId);
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "exit")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        private static int NumberOfPrimesInInterval(int min, int max)
+        {
+            int count = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}

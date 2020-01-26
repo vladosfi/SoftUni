d341641ff26e7d5420a34937a5904ef50a5b8a0b
vladosfi <sfi@abv.bg>
2020-01-26 19:38:52 +0200

    C# WEB - AsyncAwait

---
+++ C#/C# WEB/AsyncAwait/AsyncAwaitDemo/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    class Startup
+    {
+        static void Main()
+        {
+            PrintCount();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "exit")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        public static async void PrintCount()
+        {
+            var result = await NumberOfPrimesInIntervalAsync(2, 100000);
+            Console.WriteLine(result);
+        }
+
+        private static Task<int> NumberOfPrimesInIntervalAsync(int min, int max)
+        {
+            return Task.Run(() => NumberOfPrimesInInterval(min, max));
+        }
+
+        private static int NumberOfPrimesInInterval(int min, int max)
+        {
+            int count = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}

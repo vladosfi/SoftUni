978b21cae466b5a6bcbb897d9edbfd6f342575a6
vladosfi <sfi@abv.bg>
2020-05-27 20:35:11 +0300

    JS

--- JS Advanced/02.ARRAYS/05.ExtractIncreasingSubsequenceFromArray.js
+++ JS Advanced/02.ARRAYS/05.ExtractIncreasingSubsequenceFromArray.js
@@ -1,17 +1,31 @@
-function increasingSubsequence(array) {
-    const result = [];
-    result.push(array.shift());
-    let currentBiggestOne = result[0];
+// function increasingSubsequence(array) {
+//     const result = [];
+//     result.push(array.shift());
+//     let currentBiggestOne = result[0];
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] >= currentBiggestOne ) {
-            currentBiggestOne = array[i];
-            result.push(currentBiggestOne);
-        }
-    }
-    console.log(result.join('\n'));
-}
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] >= currentBiggestOne) {
+//             currentBiggestOne = array[i];
+//             result.push(currentBiggestOne);
+//         }
+//     }
+//     console.log(result.join('\n'));
+// }
 
 increasingSubsequence([1, 3, 8, 4, 10, 12, 3, 2, 24]);
 increasingSubsequence([1, 2, 3, 4]);
 increasingSubsequence([20, 3, 2, 15, 6, 1]);
+
+function increasingSubsequence(array) {
+    let currentBiggestOne = array[0];
+    const result = [];
+
+    array.map(x => {
+        if (x >= currentBiggestOne) {
+            result.push(x);
+            currentBiggestOne = x;
+        };
+
+    })
+    console.log(result.join('\n'));
+}

33266aea2358bb9befb3b5389b23c19eff0799c8
vladosfi <sfi@abv.bg>
2020-05-29 07:35:17 +0300

    JS - 7-ma

--- JS Advanced/02.ARRAYS/07.MagicMatrices.js
+++ JS Advanced/02.ARRAYS/07.MagicMatrices.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // function magicMatrices(arr) {
 //     const reducer =  (accumulator, currentValue) => accumulator + currentValue;
 //     arr.map(row => {
@@ -19,10 +21,15 @@ function isMagicMatrices(matrix) {
     let isMagicMatrix = true;
     const sum = matrix[0].reduce((acc, element) => acc += element, 0);
 
-    matrix.reduce((acc1, row, index) => {
-        row.reduce((acc, element) => acc += element, 0) === sum ? true : isMagicMatrix = false;
-    });
+    const arrayRow = [0, 0, 0];
 
+    matrix.reduce((acc1, row) => {
+        row.reduce((acc, element, indexCol) => {
+            acc += element, 0 === sum ? true : isMagicMatrix = false;
+            arrayRow[indexCol] += element;
+        });
+    });
+    console.log(arrayRow);
     // matrix.forEach(row => {
     //     row.reduce((acc, element) => acc += element) === sum ? true : isMagicMatrix = false;
     // });

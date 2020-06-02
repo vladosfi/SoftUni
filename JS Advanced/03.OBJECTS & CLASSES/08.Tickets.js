4a811e69248f31553e8a51e662893d532be4a68e
vladosfi <sfi@abv.bg>
2020-06-02 20:54:12 +0300

    JS

--- JS Advanced/03.OBJECTS & CLASSES/08.Tickets.js
+++ JS Advanced/03.OBJECTS & CLASSES/08.Tickets.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function solve(tickets, criteria){    
+    
+    
+    class Ticket {
+        constructor(destinationName, price, status){
+            this.destinationName = destinationName;
+            this.price = price;
+            this.status = status;
+        }
+    }
+}

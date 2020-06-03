b43875b89e931e23dcf8948e5f27a19ef7b55550
vladosfi <vladosfi@gmail.com>
2020-06-03 18:10:29 +0300

    JS-8

--- JS Advanced/03.OBJECTS & CLASSES/08.Tickets.js
+++ JS Advanced/03.OBJECTS & CLASSES/08.Tickets.js
@@ -1,13 +1,27 @@
 "use strict";
 
-function solve(tickets, criteria){    
-    
-    
+function solve(tickets, criteria) {
+    tickets.map(t => new Ticket(t)).sort(COMPARATOR[criteria]);
+
+    const comparator = {
+        destination: () = {},
+        price: () = {},
+        status: () = {}
+    }
     class Ticket {
-        constructor(destinationName, price, status){
+        constructor(descriptor) {
+            const [destinationName, price, status] = descriptor.split('|');
             this.destinationName = destinationName;
             this.price = price;
             this.status = status;
         }
     }
 }
+
+
+solve(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'destination'
+)

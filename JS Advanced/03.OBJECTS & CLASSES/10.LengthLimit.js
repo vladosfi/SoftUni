2468c0706abab87a048af51e9f10b3bb6288e01e
vladosfi <vladosfi@gmail.com>
2020-06-05 17:21:53 +0300

    JS-Object & Classes complete

---
+++ JS Advanced/03.OBJECTS & CLASSES/10.LengthLimit.js
@@ -0,0 +1,39 @@
+class Stringer {
+    constructor(innerString, innerLength) {
+        this.innerString = innerString;
+        this.innerLength = innerLength;
+    }
+
+    increase(length) {
+        this.innerLength = this.innerLength + length;
+    }
+
+    decrease(length) {
+        this.innerLength = this.innerLength - length;
+        if (this.innerLength < 0) { this.innerLength = 0; }
+    }
+
+    toString() {
+        if (this.innerLength <= 0) {
+            return '...';
+        } else if (this.innerLength >= this.innerString.length) {
+            return this.innerString;
+        } else {
+            return `${this.innerString.substring(0, this.innerLength)}...`;
+        }
+
+    }
+}
+
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
+
+test.decrease(3);
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+console.log(test.toString()); // ...
+
+test.increase(4);
+console.log(test.toString()); // Test
+

c84f200bc3b12f9f06c7b5325d86dd16a6fee01b
vladosfi <vladosfi@gmail.com>
2020-06-10 17:34:17 +0300

    JS-dom

--- JS Advanced/04.JS-Advanced-DOM/Exercise/Resources/06. Numpad-Calculator/solution.js
+++ JS Advanced/04.JS-Advanced-DOM/Exercise/Resources/06. Numpad-Calculator/solution.js
@@ -1,4 +1,7 @@
 function solve() {
+    document.querySelector('.clear').addEventListener('click', e=>{
+        console.log('click');
+    });
     document.querySelector('#calculator').addEventListener('click', onClick);
     
     function onClick(){

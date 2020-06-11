f0dedd0dcbe86741b6e7c1e2f79adfb7a8701deb
vladosfi <vladosfi@gmail.com>
2020-06-11 17:29:29 +0300

    JS-DOM

--- JS Advanced/04.JS-Advanced-DOM/Exercise/Resources/07. Number-Convertor/solution.js
+++ JS Advanced/04.JS-Advanced-DOM/Exercise/Resources/07. Number-Convertor/solution.js
@@ -1,4 +1,35 @@
 function solve() {
+    const binaryOption = document.createElement("option");
+    binaryOption.textContent = 'Binary';
+    binaryOption.value = 'binary';
+    const hexOption = document.createElement("option");
+    hexOption.textContent = 'Hexadecimal';
+    hexOption.value = 'hexadecimal';
+    const select = document.querySelector('#selectMenuTo');
+    select.appendChild(binaryOption);
+    select.appendChild(hexOption);
 
-    //TODO...
+
+    document.querySelector('button').addEventListener('click', e => {
+        let input = document.querySelector('#input');
+        let num = Number(input.value);
+        let result = document.querySelector('#result');
+        e = document.querySelector('#selectMenuTo');
+        selectedOption = e.options[e.selectedIndex].text;
+
+        if (selectedOption === 'Binary') {
+            result.value = convertToBinary(num);
+        } else if(selectedOption === 'Hexadecimal'){
+            result.value = convertToHex(num);
+        }
+    });
+
+    function convertToBinary(num) {
+        return num.toString(2);
+    }
+
+    function convertToHex(num) {
+        return num.toString(16).toUpperCase();
+    }
+
 }

26d2cd330b7c7d7f785722276c573dce62b20ae5
vladosfi <sfi@abv.bg>
2020-06-15 22:06:51 +0300

    JS - Fn context

--- JS Advanced/06.FUNCTION CONTEXT/Exercise/index.js
+++ JS Advanced/06.FUNCTION CONTEXT/Exercise/index.js
@@ -1,4 +1,18 @@
-function solve(){
-   // TODO
-}
+function solve() {
+   const tbody = document.querySelector('tbody');
+   const rows = tbody.querySelectorAll('tr');
+   tbody.addEventListener('click', onClick);
 
+   function onClick(e) {
+      const row = e.target.parentNode;
+
+      if (row.nodeName === 'TR') {
+         if (row.style.backgroundColor !== '') {
+            row.style.backgroundColor = '';
+         } else {
+            [...rows].forEach(r => r.style.backgroundColor = '');
+            row.style.backgroundColor = '#413f5e';
+         }
+      }
+   }
+}

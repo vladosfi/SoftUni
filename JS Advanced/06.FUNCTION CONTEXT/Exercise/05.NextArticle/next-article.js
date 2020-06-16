d7413691c94e571fac1ae59e766b2bbeda093a0c
vladosfi <vladosfi@gmail.com>
2020-06-16 18:05:24 +0300

    JS

--- JS Advanced/06.FUNCTION CONTEXT/Exercise/05.NextArticle/next-article.js
+++ JS Advanced/06.FUNCTION CONTEXT/Exercise/05.NextArticle/next-article.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const div = document.querySelector('#content');
+
+    function showNext() {
+        if (articles.length > 0) {
+            const newArticle = document.createElement("article");
+            newArticle.textContent = articles.shift();
+            div.appendChild(newArticle);
+        }
+    }
+
+    return showNext;
 }

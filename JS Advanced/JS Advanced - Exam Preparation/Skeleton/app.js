bfed0e54a6a705e82edae552a4fa96dfebcb785b
vladosfi <sfi@abv.bg>
2020-07-29 19:20:50 +0300

    JS - Online Exam practice

--- JS Advanced/JS Advanced - Exam Preparation/Skeleton/app.js
+++ JS Advanced/JS Advanced - Exam Preparation/Skeleton/app.js
@@ -1,9 +1,22 @@
 function solve() {
     //Запазване на референции към елементите, които ще манипулираме през целия живот на приложението
+    const sections = document.querySelectorAll('section');
+    const openDiv = sections.item(1).querySelectorAll('div').item(1);
+    const progressDiv = sections.item(1).querySelectorAll('div').item(1);
+    const finishedDiv = sections.item(1).querySelectorAll('div').item(1);
 
+    const inputTask = document.querySelector('#task');
+    const inputDesk = document.querySelector('#description');
+    const inputDate = document.querySelector('#date');
+
+    document.querySelector('#add').addEventListener('click', addTask);
     //Създаване на задачи (DOM елементи)
-    //Прочитаме съдържанието на формуляра и валидираме
-    //създаваме елементите
-    //закачаме функционалност
-    //добавяме елементи в DOM дървото
+    function addTask(e) {
+        e.preventDefault();
+        //Прочитаме съдържанието на формуляра и валидираме
+        const 
+        //създаваме елементите
+        //закачаме функционалност
+        //добавяме елементи в DOM дървото
+    }
 }

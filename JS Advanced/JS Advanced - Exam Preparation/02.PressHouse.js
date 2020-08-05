7096384131687c722c05770d6a8cf5c3bf648277
vladosfi <vladosfi@gmail.com>
2020-08-05 17:24:41 +0300

    JS - Advanced exam prep1 complete

--- JS Advanced/JS Advanced - Exam Preparation/02.PressHouse.js
+++ JS Advanced/JS Advanced - Exam Preparation/02.PressHouse.js
@@ -49,9 +49,36 @@ function solveClasses() {
     class BookReview extends Article {
         constructor(title, content, book) {
             super(title, content);
-            this.book = book;
 
+            this.book = book;
             this.clients = [];
+        }
+
+        addClient(clientName, orderDescription) {
+            if (this.clients.find(c => c.clientName === clientName) !== undefined) {
+                throw new Error('This client has already ordered this review.');
+            } else {
+                this.clients.push({
+                    clientName,
+                    orderDescription
+                });
+
+                return `${clientName} has ordered a review for ${this.book.name}`;
+            }
+        }
+
+        toString() {
+            let result = [
+                super.toString(),
+                `Book: ${this.book.name}`
+            ];
+
+            if (this.clients.length > 0) {
+                result.push('Orders:');
+                this.clients.forEach(c => result.push(`${c.clientName } - ${c.orderDescription}.`));
+            }
+
+            return result.join('\n');
         }
     }
 
@@ -67,14 +94,14 @@ function solveClasses() {
 
 let classes = solveClasses();
 let lorem = new classes.Article("Lorem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non tortor finibus, facilisis mauris vel…");
-console.log(lorem.toString()); 
+console.log(lorem.toString());
 //------------------------------
 let short = new classes.ShortReports("SpaceX and Javascript", "Yes, its damn true.SpaceX in its recent launch Dragon 2 Flight has used a technology based on Chromium and Javascript. What are your views on this ?", { title: "Dragon 2", author: "wikipedia.org" });
 console.log(short.addComment("Thank god they didn't use java."))
 short.addComment("In the end JavaScript's features are executed in C++ — the underlying language.")
-console.log(short.toString()); 
+console.log(short.toString());
 //------------------------------
-// let book = new classes.BookReview("The Great Gatsby is so much more than a love story", "The Great Gatsby is in many ways similar to Romeo and Juliet, yet I believe that it is so much more than just a love story. It is also a reflection on the hollowness of a life of leisure. ...", { name: "The Great Gatsby", author: "F Scott Fitzgerald" });
-// console.log(book.addClient("The Guardian", "100 symbols"));
-// console.log(book.addClient("Goodreads", "30 symbols"));
-// console.log(book.toString()); 
+let book = new classes.BookReview("The Great Gatsby is so much more than a love story", "The Great Gatsby is in many ways similar to Romeo and Juliet, yet I believe that it is so much more than just a love story. It is also a reflection on the hollowness of a life of leisure. ...", { name: "The Great Gatsby", author: "F Scott Fitzgerald" });
+console.log(book.addClient("The Guardian", "100 symbols"));
+console.log(book.addClient("Goodreads", "30 symbols"));
+console.log(book.toString()); 

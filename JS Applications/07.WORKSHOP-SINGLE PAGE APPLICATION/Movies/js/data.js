d187f49e9967a0f2a0fa6a4e0f99904ebf2ab2cb
vladosfi <vladosfi@gmail.com>
2020-09-10 17:28:04 +0300

    JS Application - SPA

---
+++ JS Applications/07.WORKSHOP-SINGLE PAGE APPLICATION/Movies/js/data.js
@@ -0,0 +1,153 @@
+function host(endpoint) {
+    return `https://api.backendless.com/86A5F958-32F7-ECB1-FF4C-4F443615C900/CD074802-0601-46DD-871E-FDCFF39A29D5/${endpoint}`;
+}
+
+const endpoints = {
+    REGISTER: 'users/register',
+    LOGIN: 'users/login',
+    LOGOUT: 'users/logout',
+    MOVIES: 'data/movies',
+    MOVIE_BY_ID: 'data/movies/',
+};
+
+async function register(username, password) {
+    return (await fetch(host(endpoints.REGISTER), {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username,
+            password
+        }),
+    })).json();
+}
+
+async function login(username, password) {
+    const result = (await fetch(host(endpoints.LOGIN), {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            login: username,
+            password
+        }),
+    })).json();
+
+    localStorage.setItem('userToken', result['user-token']);
+    localStorage.setItem('username', result.username);
+    localStorage.setItem('userId', result.onjectId);
+
+    return result;
+}
+
+async function logout() {
+    const token = localStorage.getItem('userToken');
+    if (token) {
+        return fetch(host(endpoints.LOGOUT), {
+            headers: {
+                'user-token': token
+            }
+        });
+    }
+}
+
+
+// get all movies
+async function getMovies() {
+    const token = localStorage.getItem('userToken');
+
+    if (token) {
+        return (await fetch(host(endpoints.MOVIES), {
+            headers: {
+                'user-token': token
+            }
+        })).json();
+    }
+}
+
+// get movie by ID
+async function getMoviesById(id) {
+    const token = localStorage.getItem('userToken');
+
+    if (token) {
+        return (await fetch(host(endpoints.MOVIE_BY_ID + id), {
+            headers: {
+                'user-token': token
+            }
+        })).json();
+    }
+}
+
+// create movie
+async function createMovie(movie) {
+    const token = localStorage.getItem('userToken');
+
+    if (token) {
+        return (await fetch(host(endpoints.MOVIES), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'user-token': token
+            },
+            body: JSON.stringify(movie)
+        })).json();
+    }
+}
+
+// edit movie
+async function updateMovie(id, updatedProps) {
+    const token = localStorage.getItem('userToken');
+
+    if (token) {
+        return (await fetch(host(endpoints.MOVIE_BY_ID + id), {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'user-token': token
+            },
+            body: JSON.stringify(updatedProps)
+        })).json();
+    }
+}
+
+
+// delete movie
+async function deleteMovie(id) {
+    const token = localStorage.getItem('userToken');
+
+    if (token) {
+        return (await fetch(host(endpoints.MOVIE_BY_ID + id), {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'user-token': token
+            }
+        })).json();
+    }
+}
+
+// get movies by userID
+async function getMoviesByOwner(ownerId) {
+    const token = localStorage.getItem('userToken');
+
+    if (token) {
+        // return (await fetch(host(endpoints.MOVIES + `?where=ownerId${escape(='ownerId')}`), {
+        return (await fetch(host(endpoints.MOVIES + `?where=ownerId%3D%27${ownerId}%27`), {
+            headers: {
+                'Content-Type': 'application/json',
+                'user-token': token
+            }
+        })).json();
+    }
+}
+
+
+// buy ticket
+async function buyTicket(movie) {
+    const newTickets = movie.tickets - 1;
+    const movieId = movie.objectId;
+
+    return updateMovie(movieId, { tickets: newTickets });
+}

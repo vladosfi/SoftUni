b6b8d21e1d30cb127ef486a19be714d7c95c9a60
vladosfi <vladosfi@gmail.com>
2020-11-20 16:17:08 +0200

    Angular - WS1

--- Angular/Angular - ноември 2020/WORKSHOP COMPONENTS/ng-workshop/src/app/app.module.ts
+++ Angular/Angular - ноември 2020/WORKSHOP COMPONENTS/ng-workshop/src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ThemeListComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent]
 })
 export class AppModule { }

ca1b14d8ac077e17503ab3c131e4531df5981dc6
vladosfi <vladosfi@gmail.com>
2020-12-01 17:21:39 +0200

    JS - Application - Exam prep

--- JS Applications/JS App Exam Prep/SoftWiki/SoftWiki/src/controllers/catalog.js
+++ JS Applications/JS App Exam Prep/SoftWiki/SoftWiki/src/controllers/catalog.js
@@ -1,12 +1,35 @@
 import { getAll } from "../data.js";
 import { addPartials } from "../util.js";
 
-export async function homePage() { 
+export async function homePage() {
     await addPartials(this);
+    this.partials.article = await this.load('/templates/catalog/article.hbs');
+
+    // const context = {
+    //     articles: await getAll()
+    // };
 
     const context = {
-        articles: await getAll()
-    };
+        js: [
+            {
+                title: 'Article1',
+                category: 'JavaScript',
+                content: 'Lorem ipsum ',
+            },
+            {
+                title: 'Article2',
+                category: 'JavaScript',
+                content: 'Lorem ipsum ',
+            }
+        ],
+        java: [
+            {
+                title: 'Article1',
+                category: 'java',
+                content: 'Lorem ipsum ',
+            }
+        ],
+    }
 
-    this.partial('/templates/catalog/homePage.hbs');
+    this.partial('/templates/catalog/homePage.hbs', context);
 }

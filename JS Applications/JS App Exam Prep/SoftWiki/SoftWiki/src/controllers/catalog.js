40bcfb69420d70f910de0169a0d015916ca996b8
vladosfi <vladosfi@gmail.com>
2020-12-02 17:28:58 +0200

    JS - Application exam prep

--- JS Applications/JS App Exam Prep/SoftWiki/SoftWiki/src/controllers/catalog.js
+++ JS Applications/JS App Exam Prep/SoftWiki/SoftWiki/src/controllers/catalog.js
@@ -1,35 +1,73 @@
-import { getAll } from "../data.js";
-import { addPartials } from "../util.js";
+import { getAll, createArticle, getById } from "../data.js";
+import { addPartials, mapCategories } from "../util.js";
 
 export async function homePage() {
     await addPartials(this);
     this.partials.article = await this.load('/templates/catalog/article.hbs');
 
-    // const context = {
-    //     articles: await getAll()
-    // };
+    const data = mapCategories(await getAll());
+    const context = data;
 
-    const context = {
-        js: [
-            {
-                title: 'Article1',
-                category: 'JavaScript',
-                content: 'Lorem ipsum ',
-            },
-            {
-                title: 'Article2',
-                category: 'JavaScript',
-                content: 'Lorem ipsum ',
-            }
-        ],
-        java: [
-            {
-                title: 'Article1',
-                category: 'java',
-                content: 'Lorem ipsum ',
-            }
-        ],
-    }
+    // const context = {
+    //     js: [
+    //         {
+    //             title: 'Article1',
+    //             category: 'JavaScript',
+    //             content: 'Lorem ipsum ',
+    //         },
+    //         {
+    //             title: 'Article2',
+    //             category: 'JavaScript',
+    //             content: 'Lorem ipsum ',
+    //         }
+    //     ],
+    //     java: [
+    //         {
+    //             title: 'Article1',
+    //             category: 'Java',
+    //             content: 'Lorem ipsum ',
+    //         }
+    //     ],
+    // }
+
+    context.user = this.app.userData;
 
     this.partial('/templates/catalog/homePage.hbs', context);
+}
+
+export async function detailsPage() {
+    await addPartials(this);
+
+    const article = await getById(this.params.id);
+    const context ={
+        user: this.app.userData,
+        article
+    }
+
+    this.partial('/templates/catalog/detailsPage.hbs', context);
+}
+
+export async function createPage() {
+    await addPartials(this);
+
+    const context = {
+        user: this.app.userData
+    }
+
+    this.partial('/templates/catalog/createPage.hbs', context);
+}
+
+export async function postCreate(ctx) {
+    const { title, category, content } = ctx.params;
+
+    try {
+        if (title.length == 0 || category.length == 0 || content.length == 0) {
+            throw new Error('All fields are required!');
+        } else {
+            const result = await createArticle({ title, category, content });
+            ctx.redirect('/home');
+        }
+    } catch (err) {
+        alert(err.message);
+    }
 }
